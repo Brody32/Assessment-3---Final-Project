e98d686c002d614df794742d2d23a6f4cb57593e
Brody32 <brody.mountford@hotmail.com>
2017-06-02 14:17:54 +0800

    Completed Remaining Features to be Implemented

---
+++ VehicleRentSystem/VehicleRentSystem/FormMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VehicleRentSystem
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            loadVehicles();
+        }
+
+        public Vehicle[] Vehicles = new Vehicle[3]; // holds the vehicle objects
+
+        public void loadVehicles() // Initialize the Vehicles
+        {
+            // temp object to add variables to. Honestly this seems like a terrible way of doing it but i can't do it any other way because it wont let me :x also i know nuffin
+            Vehicle VehicleTemp = new Vehicle();
+
+            // Initialize values for Vehicle 0
+            VehicleTemp.manufacturer = "Audi";
+            VehicleTemp.model = "A6";
+            VehicleTemp.production_date = 2003;
+            VehicleTemp.registration = "1 ABC 221";
+
+            VehicleTemp.total_km_travelled = 0;
+            VehicleTemp.total_litrespurchased = 0;
+            VehicleTemp.total_services = 0;
+            VehicleTemp.fuel_economy = 0;
+            VehicleTemp.service_required = false;
+            VehicleTemp.revenue = 0;
+
+            Vehicles[0] = VehicleTemp; // add to the array
+
+            // Initialize values for Vehicle 1
+            VehicleTemp.manufacturer = "Ford";
+            VehicleTemp.model = "T812";
+            VehicleTemp.production_date = 2014;
+            VehicleTemp.registration = "1 ABD 760";
+
+            VehicleTemp.total_km_travelled = 0;
+            VehicleTemp.total_litrespurchased = 0;
+            VehicleTemp.total_services = 0;
+            VehicleTemp.fuel_economy = 0;
+            VehicleTemp.service_required = false;
+            VehicleTemp.revenue = 0;
+
+            Vehicles[1] = VehicleTemp; // add to the array
+
+            // Initialize vaues for Vehicle 2
+            VehicleTemp.manufacturer = "Jaguar";
+            VehicleTemp.model = "X200";
+            VehicleTemp.production_date = 2003;
+            VehicleTemp.registration = "1 ABG 432";
+
+            VehicleTemp.total_km_travelled = 0;
+            VehicleTemp.total_litrespurchased = 0;
+            VehicleTemp.total_services = 0;
+            VehicleTemp.fuel_economy = 0;
+            VehicleTemp.service_required = false;
+            VehicleTemp.revenue = 0;
+
+            Vehicles[2] = VehicleTemp; // add to the array
+        }
+
+    }
+}
---
+++ VehicleRentSystem/VehicleRentSystem/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentSystem
+{
+    public class Vehicle
+    {
+        // First constructor
+        public Vehicle() { }
+
+        // Properties
+        public string manufacturer { get; set; }
+        public string model { get; set; }
+        public int production_date { get; set; }
+        public string registration { get; set; }
+
+        public double total_km_travelled { get; set; }
+        public double total_km_travelled_since_service { get; set; }
+        public double total_litrespurchased { get; set; }
+        public int total_services { get; set; }
+        public double fuel_economy { get; set; }
+        public bool service_required { get; set; }
+        public double revenue { get; set; }
+
+    }
+}
